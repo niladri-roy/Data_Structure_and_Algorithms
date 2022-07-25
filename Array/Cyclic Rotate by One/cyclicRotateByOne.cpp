fed8b7f919b6faff4a6366df9921d9da9d5d4ada
Niladri Roy <64502790+niladri-roy@users.noreply.github.com>
2022-07-25 21:55:29 +0530

    2nd day Commit

--- Array/Cyclic Rotate by One/cyclicRotateByOne.cpp
+++ Array/Cyclic Rotate by One/cyclicRotateByOne.cpp
@@ -1,14 +1,18 @@
 /*
 Approach 1:
+*Use two pointers approach and point the first and the last element of the array.
+*Swap the elements, and
+*Do the obvious increment.
+
+Approach 1 , Explanation:
+
 We can use two pointers,
 say i and j which point to first and last element of array respectively.
-As we know in cyclic rotation we will bring last element to first and 
+As we know in cyclic rotation we will bring last element to first and
 shift rest in forward direction,
 so start swaping arr[i] and arr[j] and
 keep j fixed and i moving towards j.
 Repeat till i is not equal to j
-
-Approach 1 , Explanation:
 */
 
 #include<iostream>
---
+++ Array/Remove Duplicates/removeDuplicates.cpp
@@ -0,0 +1,14 @@
+/*
+Approach 1 Using HashMap:
+Time Complexity: O(n)
+Space Complexity: O(n)
+Take a hash map, which will store all the elements
+which have appeared before.
+Traverse the array.
+Check if the element is present in the hash map.
+If yes, continue traversing the array.
+Else Print the element.
+
+Explanaton of Approach 1 :
+
+*/
